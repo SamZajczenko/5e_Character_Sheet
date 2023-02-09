cf612db84cf5f255474585aa634e0a24e95dac73
Sam Zajczenko <113551869+Sam786968@users.noreply.github.com>
2023-02-09 21:22:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+def character_inputs():
+  name = input()
+  strength = input()
+  dexterity = input()
+  constitution = input()
+  intelligentce = input()
+  wisdom = input()
+  charisma = input()
+  speed =input()
+
+#  races order (speed, )
+human = []
+dwarf = []
+elf = []
+
+# classes
+barbarian
+fighter
+clearic
+ranger
+druid
+bard
+monk
+paladin
+rogue 
+sorcerer
+warlock
+wizard
+artificer
+
+def level():
+  level
+  proficiency_bonus 
+  
+
+def 

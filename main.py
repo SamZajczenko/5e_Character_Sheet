619556fe1e020ebba843773e8acabf89df6a2724
Sam Zajczenko <113551869+Sam786968@users.noreply.github.com>
2023-02-18 01:31:42 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
-def character_inputs():
-  name = input()
-  strength = input()
-  dexterity = input()
-  constitution = input()
-  intelligentce = input()
-  wisdom = input()
-  charisma = input()
-  speed =input()
+import math
 
-#  races order (speed, )
+def dice():
+  
+
+def character_inputs():
+  name = input("What is your character's name? ")
+  strength = 
+  dexterity = 
+  constitution = 
+  intelligentce = 
+  wisdom = 
+  charisma = 
+  speed =
+  inspiration = 
+
+def items():
+  armour = 
+  item_list = 
+  weapons = 
+
+#  races order (class specific type, ability score increase, size, speed, darkvision)
 human = []
 dwarf = []
 elf = []
+gnome = []
+halfling = []
 
-# classes
-barbarian
-fighter
-clearic
-ranger
-druid
-bard
-monk
-paladin
-rogue 
-sorcerer
-warlock
-wizard
-artificer
+
+# classes ()
+barbarian = 
+fighter = 
+clearic = 
+ranger = 
+druid = 
+bard = 
+monk = 
+paladin = 
+rogue = 
+sorcerer = 
+warlock = 
+wizard = 
+artificer = []
 
 def level():
   level
   proficiency_bonus 
-  
 
-def 
+def modifer_system():
+  (10, +1)
+
+def main_character_builder():
+  character_inputs()
+  

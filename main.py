674d08eb5701eab4e53b286624e81eef3f213ce4
Sam <samzajc@gmail.com>
2023-03-04 19:01:26 -0600

    made changes to the overall way I am running it

--- main.py
+++ main.py
@@ -1,54 +1,109 @@
 import math
+import random
+from random import randint
 
-def dice():
+# These will be the dice the character will use. 
+def roll(num, dice):
+  if num in dice:
+    num=num.replace('d','')
+    num = randint(1,int(num))
+    print("You rolled a: " + str(num))
+  else:
+    print("That is not a valid number, pick a valid number")
+
+# variables that should be tracked
+class main_sheet():
+  def one(self, alignment, age, ):
+    self.alignment
+
+class race:
   
+  import math
+from random import randint
 
-def character_inputs():
-  name = input("What is your character's name? ")
-  strength = 
-  dexterity = 
-  constitution = 
-  intelligentce = 
-  wisdom = 
-  charisma = 
-  speed =
-  inspiration = 
+def roll(num):
+    if num.startswith('d'):
+        num = randint(1, int(num[1:]))
+        print(f"You rolled a {num}.")
+    else:
+        print("That is not a valid number, pick a valid number.")
 
-def items():
-  armour = 
-  item_list = 
-  weapons = 
+class Race:
+    def __init__(self, speed, score, darkvision, abilities, language):
+        self.speed = speed
+        self.score = score
+        self.darkvision = darkvision
+        self.abilities = abilities
+        self.language = language
 
-#  races order (class specific type, ability score increase, size, speed, darkvision)
-human = []
-dwarf = []
-elf = []
-gnome = []
-halfling = []
+class elf(Race):
+    def __init__(self, wood=False, drow=False, high=False):
+        if wood:
+            super().__init__(speed=35, score=2, darkvision=True, abilities="", language="Common, Elvish")
+        elif drow:
+            super().__init__(speed=30, score=2, darkvision=True, abilities="", language="Common, Elvish")
+        elif high:
+            super().__init__(speed=30, score=2, darkvision=False, abilities="", language="Common, Elvish")
 
+class dwarf(Race):
+    def __init__(self, hill=False, mountain=False):
+        if hill:
+            super().__init__(speed=25, score=2, darkvision=True, abilities="", language="Common, Dwarvish")
+        elif mountain
 
-# classes ()
-barbarian = 
-fighter = 
-clearic = 
-ranger = 
-druid = 
-bard = 
-monk = 
-paladin = 
-rogue = 
-sorcerer = 
-warlock = 
-wizard = 
-artificer = []
+class halfling(Race):
+  def __init__(self, speed, score, darkvision, abilities, language):
+     super().__init__(speed, score, darkvision, abilities, language) 
 
-def level():
-  level
-  proficiency_bonus 
+class human(Race):
+  def
 
-def modifer_system():
-  (10, +1)
+class dragonborn(Race):
+  def
 
-def main_character_builder():
-  character_inputs()
+class gnome(Race):
+  def
+
+class halfelf(Race):
+  def
+
+class halforc(Race):
+  def 
+
+class tiefling(Race):
+  def
   
+class Character:
+    def character(self, name, race, char_class, level, strength, dexterity, constitution, intelligence, wisdom, charisma):
+        self.name = name
+        self.race = race
+        self.char_class = char_class
+        self.level = level
+        self.strength = strength
+        self.dexterity = dexterity
+        self.constitution = constitution
+        self.intelligence = intelligence
+        self.wisdom = wisdom
+        self.charisma = charisma
+    
+    def show_stats(self):
+        print(f"Name: {self.name}")
+        print(f"Race: {self.race}")
+        print(f"Class: {self.char_class}")
+        print(f"Level: {self.level}")
+        print(f"Strength: {self.strength}")
+        print(f"Dexterity: {self.dexterity}")
+        print(f"Constitution: {self.constitution}")
+        print(f"Intelligence: {self.intelligence}")
+        print(f"Wisdom: {self.wisdom}")
+        print(f"Charisma: {self.charisma}")
+
+# Sample character
+character = Character("Gandalf", "Human", "Wizard", 10, 8, 14, 12, 18, 16, 20)
+
+# Show character stats
+character.show_stats()
+
+class Items:
+  def items(self, armor, ):
+    self.armor = armor 

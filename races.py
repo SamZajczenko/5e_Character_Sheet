841c425179b292ac7d448c7d1e691741a2eaa2f1
Sam786968 <samzajc@gmail.com>
2023-03-21 15:03:05 -0500

    fixing the code for the races, plus json

--- races.py
+++ races.py
@@ -24,61 +24,79 @@ class Race:
         self.abilities = abilities
         self.language = language
 
-
 # Dwarven race.
-class dwarf(Race):
+class Dwarf(Race):
     def __init__(self, hill=False, mountain=False):
+        abilities = ["darkvision", "dwarven resilience", "stonecunning"]
         if hill:
-            super().__init__(speed=25, score=[wisdom,1], darkvision=True, abilities=abilties[0][0], language="Common, Dwarvish")
+            super().__init__(speed=25, score=[2, 0, 2, 0, 0, -2], darkvision=True, abilities=abilities, language="Common, Dwarvish")
+            self.abilities.append("dwarven toughness")
         elif mountain:
-            super().__init__(speed=25, score=[strength,2], darkvision=True, abilities=abilties[0][0], language="Common, Dwarvish")  
-    
+            super().__init__(speed=25, score=[2, 0, 2, 0, 0, -2], darkvision=True, abilities=abilities, language="Common, Dwarvish")
+            self.abilities.append("dwarven armor training")
+        else:
+            super().__init__(speed=25, score=[2, 0, 2, 0, 0, -2], darkvision=True, abilities=abilities, language="Common, Dwarvish")
+
     dwarf_dict = {
-        "dwarf_abilities": "",
-        "hill_dwarf_abilities": "",
-        "mountain_dwarf_abilities": "",
-        }
+        "dwarf_abilities": ["darkvision", "dwarven resilience", "stonecunning"],
+        "hill_dwarf_abilities": ["darkvision", "dwarven resilience", "stonecunning", "dwarven toughness"],
+        "mountain_dwarf_abilities": ["darkvision", "dwarven resilience", "stonecunning", "dwarven armor training"],
+    }
 
     dwarf_abilities = ""
         
 
 # Elven race.
-class elf(Race):
+class Elf(Race):
     def __init__(self, wood=False, drow=False, high=False):
         if wood:
-            super().__init__(speed=35, score=2, darkvision=True, abilities="", language="Common, Elvish")
+            super().__init__(speed=35, score=[0,2,0,0,1,0], darkvision=True, abilities="", language="Common, Elvish")
         elif drow:
-            super().__init__(speed=30, score=2, darkvision=True, abilities="", language="Common, Elvish")
+            super().__init__(speed=30, score=[0,2,0,0,0,1], darkvision=True, abilities="", language="Common, Elvish")
         elif high:
-            super().__init__(speed=30, score=2, darkvision=False, abilities="", language="Common, Elvish")
+            super().__init__(speed=30, score=[0,2,0,1,0,0], darkvision=False, abilities="", language="Common, Elvish")
+        else:
+            super().__init__(speed=30, score=[0,2,0,0,0,0], darkvision=False, abilities="", language="Common, Elvish")
+    
     elf_dict = {
-        "elf_abilities": ""
-        "wood_elf_abilities": "",
-        "high_elf_abilities": "",
-        "drow_elf_abilities": "",
-        }
+        "elf_abilities": ["darkvision", "keen sense", "fey ancestry", "trance"],
+        "wood_elf_abilities": ["mask of the wild", "elf weapon training"],
+        "high_elf_abilities": ["elf weapon training","cantrip"],
+        "drow_elf_abilities": ["superior darkvision", "sunlight sensitivity", "drow magic", "drow weapon casting"],
+    }
 
     elf_abilities = ""
 
 
 # Halfling race.
-class halfling(Race):
-    def __init__()
+class Halfling(Race):
+    def __init__(self, lightfoot=False, stout=False):
+        if lightfoot:   
+            super().__init__(speed=25, score=[0,2,0,0,0,0], darkvision=False, abilities=abilities, language="Common, Halfling") 
+        if stout:
+            super().__init__(speed=25, score=[0,2,0,0,0,0], darkvision=False, abilities=abilities, language="Common, Halfling") 
+        else:
+            super().__init__(speed=25, score=[0,2,0,0,0,0], darkvision=False, abilities=abilities, language="Common, Halfling") 
     
-halfling_dict = {
-    "halfling_abilities": "",
-}
+    halfling_dict = {
+        "halfling_abilities": ["lucky"],
+        "lightfoot_halfling_abilities": , 
+        "stout_halfling_abilities": , 
+    }
 
-halfling_abilities = ""
-
-class halfling(Race):
-  def __init__(self, speed, score, darkvision, abilities, language):
-     super().__init__(speed, score, darkvision, abilities, language) 
+    halfling_abilities = ""
 
 
 # Human race.
-class human(Race):
-  def __init__()
+class Human(Race):
+  def __init__(self):
+      super().__init__(speed, score, darkvision, abilities, language)
+
+    human_dict = {
+        "human_abilities": "",
+    }
+        
+    human_abilities = ""
 
 # Dragonborn race.
 class dragonborn(Race):
